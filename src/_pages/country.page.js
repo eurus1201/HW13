3469a081b5db744bf70d6855db9a37d22ee8908d
eurus1201 <khodabakhshian.s@gmail.com>
2019-08-13 10:33:30 +0430

    add map component

--- src/_pages/country.page.js
+++ src/_pages/country.page.js
@@ -5,110 +5,85 @@ import '../_styles/App.scss';
 import world from '../world.svg';
 import Bootstrap from '../_styles/bootstrap-grid.min.css';
 // import GoogleMapReact from 'google-map-react';
+import GoogleApiWrapper from "./map";
 
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+  class Country extends Component {
 
+    constructor(props) {
+      super(props);
+      Axios.get('https://restcountries.eu/rest/v2/alpha/' + props.match.params.code).then(response => {
+        this.setState({ ...response.data })
+      })
+    }
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class Country extends Component {
-
-  constructor(props) {
-    super(props);
-    Axios.get('https://restcountries.eu/rest/v2/alpha/' + props.match.params.code).then(response => {
-      this.setState({ ...response.data })
-    })
-  }
-
-  goBack = () => {
-    this.props.history.push('/');
-  }
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-  render() {
-    const { state } = this;
-    return <>
-      <nav>
-        <img src={world} className="nav-logo" alt="logo" />
-        <span>Country Info - {this.state && this.state.name}</span>
-        <button onClick={this.goBack}>Back</button>
-      </nav>
-      {state ? <main className="container-fluid">
-        <div className="row">
-          <div className="col-12 col-lg-4"><img src={state.flag} className="flag" alt="flag" /></div>
-          <div className="col-12 col-lg-4" style={{ background: '#ffdc37', padding: 0, textAlign: 'center' }}>
-            <div style={{ background: 'black', color: 'white', textAlign: 'center', padding: '15px' }}>CALLING CODE</div>
-            <div><h1>{state.callingCodes}</h1></div>
-          </div>
-          <div className="col-12 col-lg-4">
-            <div style={{ background: '#504e4e', color: 'white', height: '100%', padding: '15px' }}>
-              <div>{state.name}</div>
-              <div>
-                <ul className="info">
-                  <li><span>Native Name:</span> {state.nativeName}</li>
-                  <li><span>Capital:</span> {state.capital}</li>
-                  <li><span>Region:</span> {state.region}</li>
-                  <li><span>Population:</span> {state.population}</li>
-                  <li><span>Languages:</span> {state.languages[0].name}</li>
-                  <li><span>TimeZone:</span> {state.timezones}</li>
-                </ul>
+    goBack = () => {
+      this.props.history.push('/');
+    }
+    static defaultProps = {
+      center: {
+        lat: 59.95,
+        lng: 30.33
+      },
+      zoom: 11
+    };
+    render() {
+      const { state } = this;
+      return <>
+        <nav>
+          <img src={world} className="nav-logo" alt="logo" />
+          <span>Country Info - {this.state && this.state.name}</span>
+          <button onClick={this.goBack}>Back</button>
+        </nav>
+        {state ? <main className="container-fluid">
+          <div className="row">
+            <div className="col-12 col-lg-4"><img src={state.flag} className="flag" alt="flag" /></div>
+            <div className="col-12 col-lg-4" style={{ background: '#ffdc37', padding: 0, textAlign: 'center' }}>
+              <div style={{ background: 'black', color: 'white', textAlign: 'center', padding: '15px' }}>CALLING CODE</div>
+              <div><h1>{state.callingCodes}</h1></div>
+            </div>
+            <div className="col-12 col-lg-4">
+              <div style={{ background: '#504e4e', color: 'white', height: '100%', padding: '15px' }}>
+                <div>{state.name}</div>
+                <div>
+                  <ul className="info">
+                    <li><span>Native Name:</span> {state.nativeName}</li>
+                    <li><span>Capital:</span> {state.capital}</li>
+                    <li><span>Region:</span> {state.region}</li>
+                    <li><span>Population:</span> {state.population}</li>
+                    <li><span>Languages:</span> {state.languages[0].name}</li>
+                    <li><span>TimeZone:</span> {state.timezones}</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-4">
-            <div style={{ background: 'black', color: 'white', textAlign: 'center', padding: '15px' }}>Capital Weather Report</div>
-            <div>
-              <span><img src="" className="flag" alt="flag" /></span>
-              <ul className="weather-info">
-                <li><span>Wind Speed:</span> {state.nativeName}</li>
-                <li><span>Temprature:</span> {state.capital}</li>
-                <li><span>Humidity:</span> {state.region}</li>
-                <li><span>Visibility:</span> {state.population}</li>
-              </ul>
+          <div className="row">
+            <div className="col-lg-4">
+              <div style={{ background: 'black', color: 'white', textAlign: 'center', padding: '15px' }}>Capital Weather Report</div>
+              <div>
+                <span><img src="" className="flag" alt="flag" /></span>
+                <ul className="weather-info">
+                  <li><span>Wind Speed:</span> {state.nativeName}</li>
+                  <li><span>Temprature:</span> {state.capital}</li>
+                  <li><span>Humidity:</span> {state.region}</li>
+                  <li><span>Visibility:</span> {state.population}</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <div className="map">
+                < GoogleApiWrapper lat={state.lat} lng={state.lng} />
+              </div>
             </div>
           </div>
-          <div className="col-lg-8">
-            <div style={{ height: '100vh', width: '100%' }}>
-              {/* <GoogleMapReact
-                bootstrapURLKeys={{ key: "AIzaSyAdj0C81rA_-Ko03ne6H63lfpvKS-vRNR4",
-              }}
-                defaultCenter={this.props.center}
-                defaultZoom={this.props.zoom}
-              >
-                <AnyReactComponent
-                  lat={59.955413}
-                  lng={30.337844}
-                  text="My Marker"
-                />
-              </GoogleMapReact> */}
-            
-              <Map google={window.google}
-                initialCenter={{}}
-                zoom={14} /> />
-            
-            
-              
 
-            
-             
-             
-                         
-            </div>
-          </div>
-          </div>
-        
-      </main> : <span>Loading...</span>}
-    </>
+        </main> : <span>Loading...</span>}
+      </>
+    }
+
   }
 
-}
+//   apiKey: 'AIzaSyAdj0C81rA_-Ko03ne6H63lfpvKS-vRNR4'
 
-withRouter(Country);
-export { Country };
+withRouter(Country)
+export { Country }
---
+++ src/_pages/map.js
@@ -0,0 +1,39 @@
+import React , { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+export class MapContainer extends Component {
+
+constructor (props){
+    super(props);
+    
+}
+
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+             
+                center={{
+                    lat: {lat},
+                    lng: {lng}
+                }}
+                zoom={14}
+                onClick={this.onMapClicked}
+            >
+
+                <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyAdj0C81rA_-Ko03ne6H63lfpvKS-vRNR4')
+})
